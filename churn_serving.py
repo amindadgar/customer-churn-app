f8937199bbb01dda1ca749262f43116f17347046
Amin <dadgaramin96@gmail.com>
2021-10-09 08:58:06 +0330

    Added test route

--- churn_serving.py
+++ churn_serving.py
@@ -30,6 +30,9 @@ def predict():
 
     return jsonify(result)
 
+@app.route('/test',methods = ['GET'])
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
